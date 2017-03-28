f631d6e135c247a4ce256b2e4a9a8612c5318b7b
Zalan Toth <zlaval@gmail.com>
2017-03-29 00:21:24 +0200

    refactor and add eureka server

--- config-server/src/main/java/com/zlrx/javadevzed/microservice/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/zlrx/javadevzed/microservice/config/ConfigServerApplication.java
@@ -1,16 +1,19 @@
-package com.zlrx.javadevzed.microservice;
+package com.zlrx.javadevzed.microservice.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
-//http://localhost:8001/config-server/default/
+/**
+ * http://localhost:8001/config-server/default/
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
 }
---
+++ eureka-server/src/main/java/com/zlrx/javadevzed/microservice/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.zlrx.javadevzed.microservice.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * http://localhost:8010
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
