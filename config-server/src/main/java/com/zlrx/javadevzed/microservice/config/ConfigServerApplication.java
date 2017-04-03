125b3a24dc4fb70e0fe7c1ef2c29efb32d5885a3
Zalan Toth <zlaval@gmail.com>
2017-04-03 22:01:39 +0200

    refactor eureka server

--- config-server/src/main/java/com/zlrx/javadevzed/microservice/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/zlrx/javadevzed/microservice/config/ConfigServerApplication.java
@@ -11,9 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
